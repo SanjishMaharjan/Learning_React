0534f5cdb7be057830682eea434e1f59d80f4182
Sanjish Maharjan <84065079+SanjishMaharjan@users.noreply.github.com>
2022-12-26 10:23:24 +0545

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+ReactDom.render(
+  <div>
+    <h1>Hello I am H1</h1>
+    <p>Hellow I am P</p>
+  </div>,
+  document.getElementById("root")
+);
